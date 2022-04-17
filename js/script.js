6308a59ddc88688378133c063e9b1d5fd71fda41
Zergarus <47299260+Zergarus@users.noreply.github.com>
2022-04-17 18:01:23 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(function() {
+    // Owl Carousel
+    var owl = $(".owl-carousel");
+    
+    owl.owlCarousel({
+      items: 4,
+      margin: 10,
+      loop: true,
+      nav: true,
+      navText : ["<i class='arrow-left'><img src='images/next 24.png' /></i>","<i class='arrow-right'><img src='images/next 23.png' /></i>"]
+    });
+  });
+
