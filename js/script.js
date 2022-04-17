db2a6fc37235af081d5ae5d01998341b50dc9684
Zergarus <47299260+Zergarus@users.noreply.github.com>
2022-04-17 22:52:04 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
-$(function() {
-    // Owl Carousel
-    var owl = $(".owl-carousel");
-    
-    owl.owlCarousel({
-      items: 4,
-      margin: 10,
-      loop: true,
-      nav: true,
-      navText : ["<i class='arrow-left'><img src='images/next 24.png' /></i>","<i class='arrow-right'><img src='images/next 23.png' /></i>"]
+$(document).ready(function(){
+    $('.owl-one').owlCarousel({
+        items: 4,
+        loop:true,
+        margin:10,
+        nav:true,
+        navText : ["<i class='arrow-left'><img src='images/next 24.png' /></i>","<i class='arrow-right'><img src='images/next 23.png' /></i>"]
     });
-  });
 
+    $('.owl-two').owlCarousel({
+        items: 6,
+        loop:true,
+        margin:10,
+        nav:true,
+        navText : ["<i class='arrow-left'><img src='images/next 24.png' /></i>","<i class='arrow-right'><img src='images/next 23.png' /></i>"]
+    });
+});
